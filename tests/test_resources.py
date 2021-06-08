bd9f99859461976bbe514e5cbe07598bbc16e19e
Edward Popoola <edward@cowrywise.com>
2021-06-08 01:50:30 +0100

    Add tests and new transfer endpoints

--- tests/test_resources.py
+++ tests/test_resources.py
@@ -1,22 +1,2 @@
 import pytest
 from embed.client import Client
-
-# @pytest.mark.parametrize(
-#     "resource_name",
-#     [
-#         "Accounts",
-#         "Links",
-#         "Transactions",
-#         "Balances",
-#         "Owners",
-#         "Institutions",
-#         "Incomes",
-#         "Invoices",
-#         "TaxReturns",
-#         "Statements",
-#         "WidgetToken",
-#     ],
-# )
-# def test_client_resources_uses_same_session_as_client(resource_name):
-#     c = Client(client_id="a", client_secret="b")
-#     assert c.session is getattr(c, resource_name).session
