efa2d02474577b71e48e979199f76a8a4912d16f
Uchechukwu Emmanuel <daleentontech@gmail.com>
2021-07-06 11:53:05 +0100

    included tests for /deposts and /withdrawals

--- tests/resources/test_transfers.py
+++ tests/resources/test_transfers.py
@@ -16,6 +16,26 @@ def test_can_get_transfers(mock_get_essential_details, api_session):
         f"{api_session.base_url}/api/{api_session.api_version}/transfers",
     )
 
+@patch('embed.common.APIResponse.get_essential_details')
+def test_can_get_deposits(mock_get_essential_details, api_session):
+    transfer = Transfer(api_session)
+    mock_get_essential_details.return_value = MagicMock()
+    transfer.get_deposits()
+    transfer.get_essential_details.assert_called_with(
+        "GET",
+        f"{api_session.base_url}/api/{api_session.api_version}/deposits",
+    )
+
+@patch('embed.common.APIResponse.get_essential_details')
+def test_can_get_withdrawals(mock_get_essential_details, api_session):
+    transfer = Transfer(api_session)
+    mock_get_essential_details.return_value = MagicMock()
+    transfer.get_withdrawals()
+    transfer.get_essential_details.assert_called_with(
+        "GET",
+        f"{api_session.base_url}/api/{api_session.api_version}/withdrawals",
+    )
+
 
 @patch('embed.common.APIResponse.get_essential_details')
 def test_can_get_single_transfer(mock_get_essential_details, api_session):
