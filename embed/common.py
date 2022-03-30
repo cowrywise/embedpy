b7013f3ed321428f276c97486c00269731f760c6
cAtaman <chimaataman@gmail.com>
2022-03-30 14:27:48 +0100

    fix magic method `__str__` of APISession

--- embed/common.py
+++ embed/common.py
@@ -133,4 +133,4 @@ class APISession(APIResponse):
         return self._base_url
 
     def __str__(self):
-        return f"{self._base_url} - {self._token} - {self._api_version}"
+        return f"{self._base_url} - {self.token} - {self._api_version}"
