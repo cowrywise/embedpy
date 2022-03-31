f10e0ca33e8f701e97b21f73539ffe494f631337
cAtaman <chimaataman@gmail.com>
2022-03-31 11:11:05 +0100

    add methods to saving.py

--- embed/resources/saving.py
+++ embed/resources/saving.py
@@ -1,4 +1,6 @@
 import json
+from datetime import datetime
+
 from embed.common import APIResponse
 from embed.errors import ValidationError
 
@@ -46,3 +48,69 @@ class Saving(APIResponse):
         method = "GET"
         url = self.base_url + f"savings/{savings_id}"
         return self.get_essential_details(method, url)
+
+    def get_savings_rates(self, days):
+        method = "POST"
+        url = self.base_url + f"savings/rates"
+        payload = json.dumps({"days": days})
+        return self.get_essential_details(method, url, payload)
+
+    def get_savings_returns(
+        self,
+        savings_id,
+        start_date: str = None,
+        end_date: str = None
+    ):
+        params = {}
+        try:
+            if start_date:
+                datetime.strptime(start_date, "%Y-%m-%d")
+                params["start_date"] = start_date
+            if end_date:
+                datetime.strptime(end_date, "%Y-%m-%d")
+                params["end_date"] = end_date
+        except Exception:
+            ValidationError(f"start_date and end_date should be in `YYYY-MM-DD` format.")
+
+        method = "GET"
+        url = self.base_url + f"savings/{savings_id}/returns"
+        query_path = "&".join("{}={}".format(k, v) for k, v in params.items())
+        if query_path:
+            url = f"{url}?{query_path}"
+        return self.get_essential_details(method, url)
+
+    def get_savings_performance(
+        self,
+        savings_id,
+        start_date: str = None,
+        end_date: str = None
+    ):
+        params = {}
+        try:
+            if start_date:
+                datetime.strptime(start_date, "%Y-%m-%d")
+                params["start_date"] = start_date
+            if end_date:
+                datetime.strptime(end_date, "%Y-%m-%d")
+                params["end_date"] = end_date
+        except Exception:
+            ValidationError(f"start_date and end_date should be in `YYYY-MM-DD` format.")
+
+        method = "GET"
+        url = self.base_url + f"savings/{savings_id}/performance"
+        query_path = "&".join("{}={}".format(k, v) for k, v in params.items())
+        if query_path:
+            url = f"{url}?{query_path}"
+        return self.get_essential_details(method, url)
+
+    def withdraw(self, savings_id, amount):
+        method = "POST"
+        url = self.base_url + f"savings/{savings_id}/withdraw"
+        payload = json.dumps({"amount": amount})
+        return self.get_essential_details(method, url, payload)
+
+    def rollover(self, savings_id, days):
+        method = "POST"
+        url = self.base_url + f"savings/{savings_id}/rollover"
+        payload = json.dumps({"days": days})
+        return self.get_essential_details(method, url, payload)
